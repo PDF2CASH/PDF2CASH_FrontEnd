d3b017699f7c6398f19227371d3dd30313ad00e5
Wictor Bastos Girardi <wictor.girardi@gmail.com>
2018-10-04 16:17:04 -0300

    Create worker index page
    refs: fga-eps-mds/2018.2-PDF2CASH#95
    
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

---
+++ pdf2cash_front/pages/worker/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import CustomatizedTable from '../../comps/tableWorker';
+import { TableRow, TableCell, Button, Grid } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = theme => ({
+  cell: {
+    textAlign: 'center'
+  }
+});
+
+class WorkerIndex extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      workers: []
+    };
+    this.delete = this.delete.bind(this);
+  }
+
+  async componentDidMount() {
+    const url = 'http://localhost:8000/api/worker/worker/';
+    const res = await fetch(url);
+    const data_workers = await res.json();
+    this.setState({ workers: data_workers });
+  }
+
+  getWorkers() {
+    const url = 'http://localhost:8000/api/worker/worker/';
+    const res = fetch(url);
+    const data_workers = res.json();
+    this.setState({ workers: data_workers });
+  }
+
+  delete(id){
+    const url = 'http://localhost:8000/api/worker/worker/'+ id + '/';
+    const res = fetch(url, { method:'DELETE' });
+    // const data_workers =  res.json();
+    //this.getWorkers();
+  }
+    render() {
+      const { classes } = this.props;
+      return (
+        <Grid>
+          <Typography variant="display2">
+            Listar Funcionarios
+          </Typography>
+          <CustomatizedTable>
+            {
+              this.state.workers.map(worker => (
+                <TableRow key={worker.id}>
+                  <TableCell className={classes.cell}>
+                    <Typography>
+                      {worker.name}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className={classes.cell}>
+                    <Typography>
+                      {worker.cpf}
+                    </Typography>
+                  </TableCell>
+                  <TableCell className={classes.cell}>
+                    <Button>
+                      <VisibilityIcon />
+                    </Button>
+                  </TableCell>
+                  <TableCell className={classes.cell}>
+                    <Button onClick={() => this.delete(worker.id)}>
+                      <DeleteIcon />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            }
+          </CustomatizedTable>
+        </Grid>
+      );
+    }
+}
+
+export default withStyles(styles)(WorkerIndex);
