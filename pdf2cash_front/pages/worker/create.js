b4cb963897e14d6caf0d7b7dfb68ef8d0b79cb12
Lucas Silva <lucas.gomesgs0@gmail.com>
2018-10-10 13:19:56 -0300

    Add notifications when an error occurs in the registration
    refs: fga-eps-mds/2018.2-PDF2CASH#96
    
    Co-authored-by: rafaelteodosio <rafzteodosio@gmail.com>

--- pdf2cash_front/pages/worker/create.js
+++ pdf2cash_front/pages/worker/create.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import { ValidatorComponent } from 'react-form-validator-core';
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
 
 
 const styles = theme => ({
-    cell: {
-        textAlign: 'center'
+    snackbar: {
+        margin: theme.spacing.unit,
     }
 });
 
@@ -31,7 +29,7 @@ class WorkerCreate extends Component {
     }
 
     async componentDidMount() {
-      document.title = "Cadastrar funcionário";
+        document.title = "Cadastrar funcionário";
     }
 
     handleChangeName(event) {
@@ -59,77 +57,116 @@ class WorkerCreate extends Component {
         });
     }
 
+    snackbarShow() {
+
+
+    }
+
     async handleSubmit(event) {
-      event.preventDefault();
-      const url_worker = 'http://0.0.0.0:8000/api/worker/worker/';
-      fetch(url_worker, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            name: this.state.name,
-            cpf: this.state.cpf,
-            email: this.state.email,
-            password: this.state.password
-          })
-      })
-        .then((response) => {
-          if(response.status === 201){
-            window.location.href = "http://localhost:3000/worker/";
-          }
+        event.preventDefault();
+        const url_worker = 'http://0.0.0.0:8000/api/worker/worker/';
+        fetch(url_worker, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: this.state.name,
+                cpf: this.state.cpf,
+                email: this.state.email,
+                password: this.state.password
+            })
         })
+            .then((response) => {
+                if (response.ok) {
+                    window.location.href = "http://localhost:3000/worker/";
+                }
+                else
+                    return response.json()
+
+                        .then(json => {
+                            var errors = []
+                            if (json.name) {
+                                for (var i = 0; i < json.name.length; i++) {
+                                    errors.push(json.name[i])
+                                }
+                            }
+                            if (json.cpf) {
+                                for (var i = 0; i < json.cpf.length; i++) {
+                                    errors.push("Este CPF já está cadastrado no sistema.")
+                                }
+                            }
+                            if (json.email) {
+                                for (var i = 0; i < json.email.length; i++) {
+                                    errors.push("Este E-mail já está cadastrado no sistema.")
+                                }
+                            }
+                            if (json.password) {
+                                for (var i = 0; i < json.password.length; i++) {
+                                    errors.push(json.password[i])
+                                }
+                            }
+                            this.setState({ errors: errors, error_show: true });
+                        })
+            })
     }
 
     render() {
+        const { classes } = this.props;
+
         return (
-          <ValidatorForm
-            ref="form"
-            onSubmit={this.handleSubmit}
-            onError={errors => console.log(errors)}
-          >
-              <h2>CADASTRAR FUNCIONÁRIO</h2>
-              <div className="form_worker">
-                  <TextValidator
-                      label="Nome"
-                      onChange={this.handleChangeName}
-                      name="name"
-                      value={this.state.name}
-                      validators={['required', 'minStringLength:2']}
-                      errorMessages={['Este campo é obrigatório', 'Digite um nome válido']}
-                  /><br/>
-                  <TextValidator
-                      label="CPF"
-                      onChange={this.handleChangeCPF}
-                      name="cpf"
-                      inputProps={{ maxLength: 11 }}
-                      value={this.state.cpf}
-                      validators={['required', 'matchRegexp:^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$']}
-                      errorMessages={['Este campo é obrigatório', 'Digite um CPF válido']}
-                  /><br/>
-                  <TextValidator
-                      label="E-mail"
-                      onChange={this.handleChangeEmail}
-                      name="email"
-                      value={this.state.email}
-                      validators={['required', 'isEmail']}
-                      errorMessages={['Este campo é obrigatório', 'Este e-mail não é válido']}
-                  /><br/>
-                  <TextValidator
-                      label="Senha"
-                      onChange={this.handleChangePassword}
-                      name="password"
-                      type='password'
-                      value={this.state.password}
-                      validators={['required','minStringLength:6', 'maxStringLength:30']}
-                      errorMessages={['Este campo é obrigatório', 'Digite uma senha maior que 6 dígitos', 'Digite uma senha menor que 30 dígitos']}
-                  /><br/>
-              </div>
-              <Button type="submit" variant="contained" color="primary" >
-                  CADASTRAR
-              </Button>
-          </ValidatorForm>
+            <ValidatorForm
+                ref="form"
+                onSubmit={this.handleSubmit}
+                onError={errors => console.log(errors)}
+            >
+                <h2>CADASTRAR FUNCIONÁRIO</h2>
+                {
+                    this.state.error_show && this.state.errors.map(error => {
+                        return < SnackbarContent key={error} className={classes.snackbar} message={error} />
+                    })
+                }
+                <div className="form_worker">
+                    <TextValidator
+                        label="Nome"
+                        onChange={this.handleChangeName}
+                        name="name"
+                        value={this.state.name}
+                        validators={['required', 'minStringLength:9']}
+                        errorMessages={['Este campo é obrigatório', 'Digite um nome válido']}
+                    /><br />
+                    <TextValidator
+                        label="CPF"
+                        onChange={this.handleChangeCPF}
+                        name="cpf"
+                        inputProps={{ maxLength: 11 }}
+                        value={this.state.cpf}
+                        validators={['required', 'matchRegexp:^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$']}
+                        errorMessages={['Este campo é obrigatório', 'Digite um CPF válido']}
+                    /><br />
+                    <TextValidator
+                        label="E-mail"
+                        onChange={this.handleChangeEmail}
+                        name="email"
+                        value={this.state.email}
+                        validators={['required', 'isEmail']}
+                        errorMessages={['Este campo é obrigatório', 'Este e-mail não é válido']}
+                    /><br />
+                    <TextValidator
+                        label="Senha"
+                        onChange={this.handleChangePassword}
+                        name="password"
+                        type='password'
+                        value={this.state.password}
+                        validators={['required', 'minStringLength:6', 'maxStringLength:30']}
+                        errorMessages={['Este campo é obrigatório', 'Digite uma senha maior que 6 dígitos', 'Digite uma senha menor que 30 dígitos']}
+                    /><br />
+                </div>
+                <Button type="submit" variant="contained" color="primary" >
+                    CADASTRAR
+                </Button>
+            </ValidatorForm>
         );
     }
 }
