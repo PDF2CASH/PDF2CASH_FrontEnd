f73f5b033e71b8db27f128bc930a0000468afe8c
Daniel Marques <danielmarques7@hotmail.com>
2018-10-12 15:27:20 +0000

    Add form of InvoiceCreate
    refs: fga-eps-mds/2018.2-PDF2CASH#76
    
    Co-authored-by: Wictorgirardi <wictor.girardi@gmail.com>i

--- pdf2cash_front/pages/invoice/create.js
+++ pdf2cash_front/pages/invoice/create.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-
+  input: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 400,
+  }
 });
 
 class InvoiceCreate extends Component {
@@ -11,14 +15,42 @@ class InvoiceCreate extends Component {
     this.state = {
       file: null,
       open: false
-    }
+    };
+    this.setFile = this.setFile.bind(this);
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  setFile(event) {
+    this.setState({ file: event.target.files[0] });
   }
 
   render() {
     const { classes } = this.props;
 
     return (
-      <div />
+      <Grid>
+        <Typography variant='h2'>
+          Criar Nota Fiscal
+        </Typography>
+        <br /><br />
+        <form onSubmit={this.sendForm}>
+          <Input
+            accept='image/*'
+            id='raised-button-file'
+            className={classes.input}
+            onChange={this.setFile}
+            type='file'
+          />
+          <br /><br />
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+          >
+            Criar
+          </Button>
+        </form>
+      </Grid>
     );
   }
 }
