63cc7ed0c0faed62bd85f7a5b53013746576ba66
Daniel Marques <danielmarques7@hotmail.com>
2018-10-14 01:47:40 +0000

    Add embbed iframe of metabase (dashboard)
    Co-authored-by: Wictorgirardi <wictor.girardi@gmail.com>
    Co-authored-by: Brian2397 <brianlui2387@gmail.com>
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>
    
    refs fga-eps-mds/2018.2-PDF2CASH#104 fga-eps-mds/2018.2-PDF2CASH#105

--- pdf2cash_front/pages/index.js
+++ pdf2cash_front/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 
 const styles = theme => ({
 });
@@ -13,7 +14,16 @@ class Index extends React.Component {
 
     render() {
 
-        return (<h1> Hello World </h1>);
+        return (
+            <iframe
+                src="http://localhost:3030/public/dashboard/2c1a7ad7-8c66-4e6d-8493-73cb5cad0b13"
+                frameborder="0"
+                width="100%"
+                height="90%"
+                allowtransparency
+            />
+        );
+
     }
 
 }
