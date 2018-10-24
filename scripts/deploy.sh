b110529f687d12dfd9270620410cfd9cdba1322d
Wallacy Braz <wallacy.braz@live.com>
2018-10-24 18:57:57 -0300

    Change registry from aws to docker hub

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,9 @@ set -ev
 
 echo "Deployment init"
 
-pip install awscli
-export PATH=$PATH:$HOME/.local/bin
-echo $(aws ecr get-authorization-token --region us-east-2 --output text --query 'authorizationData[].authorizationToken' | base64 -d | cut -d: -f2) | docker login -u AWS https://742466423033.dkr.ecr.us-east-2.amazonaws.com --password-stdin
-docker tag pdf2front:latest 742466423033.dkr.ecr.us-east-2.amazonaws.com/pdf2cash:pdf2front
-docker push 742466423033.dkr.ecr.us-east-2.amazonaws.com/pdf2cash:pdf2front
-
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag pdf2front pdf2cash/pdf2front:latest
+docker push pdf2cash/pdf2front:latest
 
 
 
