74a28eeb0940889f6f4750ef4d9e65fc1028edfc
Wictor Bastos Girardi <wictor.girardi@gmail.com>
2018-11-02 15:01:41 -0300

    Create of a admin register page
    refs: fga-eps-mds/2018.2-PDF2CASH#129
    
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

---
+++ pdf2cash_front/pages/admin.js
@@ -0,0 +1,189 @@
+import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+
+const styles = theme => ({
+    root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    textAlign: 'center',
+    maxWidth: '30%',
+    maxWeight: '100%',
+    marginLeft: '32%',
+    marginTop: '10%',
+  },
+   grid: {
+     margin: '5%',
+   },
+   button: {
+     marginTop: '10%'
+   }
+});
+
+class AdminCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            cpf: "",
+            email: "",
+            password: "",
+            adminExists: false,
+        };
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeCPF = this.handleChangeCPF.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    async componentDidMount() {
+        document.title = "Cadastrar funcionário";
+    }
+
+    handleChangeName(event) {
+
+        this.setState({
+            name: event.target.value,
+        });
+    }
+
+    handleChangeCPF(event) {
+        this.setState({
+            cpf: event.target.value,
+        });
+    }
+
+    handleChangeEmail(event) {
+        this.setState({
+            email: event.target.value,
+        });
+    }
+
+    handleChangePassword(event) {
+        this.setState({
+            password: event.target.value,
+        });
+    }
+    validateAdmin(){
+      const length = this.state.workers.length;
+      const workers = this.state.workers;
+      for (var i = 0; i < length; i++) {
+        if(workers[i].permission === '2'){
+          this.setState({adminExists: true});
+        }
+      }
+    }
+
+    async componentDidMount() {
+      const url = 'http://localhost:8008/api/worker/worker/';
+      const res = await fetch(url);
+      const data = await res.json();
+      this.setState({
+        workers: data,
+      });
+      this.validateAdmin;
+    }
+
+    handleSubmit(event){
+      event.preventDefault();
+
+      const url_worker = 'http://localhost:8000/api/worker/worker/';
+      fetch(url_worker, {
+          method: 'POST',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              name: this.state.name,
+              cpf: this.state.cpf,
+              email: this.state.email,
+              password: this.state.password,
+              permission:  '2',
+          })
+      })
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+        <div>
+          <Paper className={classes.root} elevation={5}>
+          <Grid className={classes.grid}>
+            <ValidatorForm
+                ref="form"
+                onSubmit={this.handleSubmit}
+                onError={errors => console.log(errors)}
+            >
+            <Typography variant="h4" color="inherit" className={classes.grow}>
+                Cadastrar
+            </Typography>
+                {
+                  this.state.adminExists ? (
+                    <SnackbarContent key={'error'} className={classes.snackbar} message={'Administrador ja existe!'} />
+                  ) : (
+                    <div />
+                  )
+                }
+                <div className="form_worker">
+                    <TextValidator
+                        label="Nome"
+                        onChange={this.handleChangeName}
+                        name="name"
+                        maxWeight="40%"
+                        value={this.state.name}
+                        validators={['required', 'minStringLength:9']}
+                        errorMessages={['Este campo é obrigatório', 'Digite um nome válido']}
+                    /><br />
+                    <TextValidator
+                        label="CPF"
+                        onChange={this.handleChangeCPF}
+                        name="cpf"
+                        maxWeight="40%"
+                        inputProps={{ maxLength: 11 }}
+                        value={this.state.cpf}
+                        validators={['required', 'matchRegexp:^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$']}
+                        errorMessages={['Este campo é obrigatório', 'Digite um CPF válido']}
+                    /><br />
+                    <TextValidator
+                        label="E-mail"
+                        onChange={this.handleChangeEmail}
+                        name="email"
+                        maxWidth="40%"
+                        value={this.state.email}
+                        validators={['required', 'isEmail']}
+                        errorMessages={['Este campo é obrigatório', 'Este e-mail não é válido']}
+                    /><br />
+                    <TextValidator
+                        label="Senha"
+                        onChange={this.handleChangePassword}
+                        name="password"
+                        type='password'
+                        maxWeight="40%"
+                        value={this.state.password}
+                        validators={['required', 'minStringLength:6', 'maxStringLength:30']}
+                        errorMessages={['Este campo é obrigatório', 'Minimo de 6 dígitos', 'Digite uma senha menor que 30 dígitos']}
+                    /><br />
+                </div>
+                <Button className={classes.button} type="submit" variant="contained" color="primary" >
+                    CADASTRAR
+                </Button>
+            </ValidatorForm>
+            </Grid>
+          </Paper>
+        </div>
+        );
+    }
+}
+
+export default withStyles(styles)(AdminCreate);
