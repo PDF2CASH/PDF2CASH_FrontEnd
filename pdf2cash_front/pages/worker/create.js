232b5aef2224d0bc6dea7068129c8bd96bfb428d
Lucas Silva <lucas.gomesgs0@gmail.com>
2018-11-07 02:28:33 -0200

    Refactor worker's create file
    refs: fga-eps-mds/2018.2-PDF2CASH#139
    
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

--- pdf2cash_front/pages/worker/create.js
+++ pdf2cash_front/pages/worker/create.js
@@ -55,24 +55,23 @@ class WorkerCreate extends Component {
     });
   }
 
-  snackbarShow() {
-
-  }
-
   async handleSubmit(event) {
+    const {
+      name, cpf, email, password,
+    } = this.state;
     event.preventDefault();
-    const url_worker = 'http://0.0.0.0:8008/api/worker/worker/';
-    fetch(url_worker, {
+    const urlWorker = 'http://0.0.0.0:8008/api/worker/worker/';
+    fetch(urlWorker, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        name: this.state.name,
-        cpf: this.state.cpf,
-        email: this.state.email,
-        password: this.state.password,
+        name,
+        cpf,
+        email,
+        password,
       }),
     })
       .then((response) => {
@@ -84,50 +83,60 @@ class WorkerCreate extends Component {
             .then((json) => {
               const errors = []
               if (json.name) {
-                for (var i = 0; i < json.name.length; i++) {
+                for (let i = 0; i < json.name.length; i += 1) {
                   errors.push(json.name[ i ])
                 }
               }
               if (json.cpf) {
-                for (var i = 0; i < json.cpf.length; i++) {
+                for (let i = 0; i < json.cpf.length; i += 1) {
                   errors.push('Este CPF já está cadastrado no sistema.')
                 }
               }
               if (json.email) {
-                for (var i = 0; i < json.email.length; i++) {
+                for (let i = 0; i < json.email.length; i += 1) {
                   errors.push('Este E-mail já está cadastrado no sistema.')
                 }
               }
               if (json.password) {
-                for (var i = 0; i < json.password.length; i++) {
+                for (let i = 0; i < json.password.length; i += 1) {
                   errors.push(json.password[ i ])
                 }
               }
-              this.setState({ errors, error_show: true });
+              this.setState({ errors, errorShow: true });
             })
         }
+        return false;
       })
   }
 
   render() {
     const { classes } = this.props;
-
+    const {
+      name, email, cpf, password, errorShow, errors,
+    } = this.state;
     return (
         <ValidatorForm
-          ref="form"
           onSubmit={ this.handleSubmit }
-          onError={ errors => console.log(errors) }
+          onError={ error => console.log(error) }
         >
             <h2>CADASTRAR FUNCIONÁRIO</h2>
             {
-                    this.state.error_show && this.state.errors.map(error => <SnackbarContent key={ error } className={ classes.snackbar } message={ error } />)
+                    errorShow && errors.map(
+                      error => (
+                          <SnackbarContent
+                            key={ error }
+                            className={ classes.snackbar }
+                            message={ error }
+                          />
+                      ),
+                    )
                 }
             <div className="form_worker">
                 <TextValidator
                   label="Nome"
                   onChange={ this.handleChangeName }
                   name="name"
-                  value={ this.state.name }
+                  value={ name }
                   validators={ [ 'required', 'minStringLength:9' ] }
                   errorMessages={ [ 'Este campo é obrigatório', 'Digite um nome válido' ] }
                 />
@@ -137,7 +146,7 @@ class WorkerCreate extends Component {
                   onChange={ this.handleChangeCPF }
                   name="cpf"
                   inputProps={ { maxLength: 11 } }
-                  value={ this.state.cpf }
+                  value={ cpf }
                   validators={ [ 'required', 'matchRegexp:^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$' ] }
                   errorMessages={ [ 'Este campo é obrigatório', 'Digite um CPF válido' ] }
                 />
@@ -146,7 +155,7 @@ class WorkerCreate extends Component {
                   label="E-mail"
                   onChange={ this.handleChangeEmail }
                   name="email"
-                  value={ this.state.email }
+                  value={ email }
                   validators={ [ 'required', 'isEmail' ] }
                   errorMessages={ [ 'Este campo é obrigatório', 'Este e-mail não é válido' ] }
                 />
@@ -156,7 +165,7 @@ class WorkerCreate extends Component {
                   onChange={ this.handleChangePassword }
                   name="password"
                   type="password"
-                  value={ this.state.password }
+                  value={ password }
                   validators={ [ 'required', 'minStringLength:6', 'maxStringLength:30' ] }
                   errorMessages={ [ 'Este campo é obrigatório', 'Digite uma senha maior que 6 dígitos', 'Digite uma senha menor que 30 dígitos' ] }
                 />
