262b407bfd5e3c9dfef8d88e43f0b3147bc07a31
Rafael Teodosio <rafzteodosio@gmail.com>
2018-11-09 16:44:17 -0200

    Acceptance test with worker invalid
    refs: fga-eps-mds/2018.2-PDF2CASH#131
    
    Co-authored-by: danielmarques28 <danielmarques7@hotmail.com>

--- pdf2cash_front/src/tests/worker.create.test.js
+++ pdf2cash_front/src/tests/worker.create.test.js
@@ -48,4 +48,36 @@ describe('on register admin page', () => {
     await expect(html).toMatch('Listar Funcionarios');
     browser.close();
   }, 160000);
+
+  test('with worker invalid', async() => {
+    let browser = await puppeteer.launch({args: ['--no-sandbox']});
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: ''
+    });
+
+    await page.goto(APP);
+    let html = await page.evaluate(() => document.body.innerHTML);
+    expect(html).toMatch('CADASTRAR FUNCIONÁRIO');
+    await page.goto(APP);
+    await page.click("input[name=name]");
+    await page.type("input[name=name]", lead_fail.name);
+    await page.click("input[name=email]");
+    await page.type("input[name=email]", lead_fail.email);
+    await page.click("input[name=cpf]");
+    await page.type("input[name=cpf]", lead_fail.cpf);
+    await page.click("input[name=password]");
+    await page.type("input[name=password]", lead_fail.password);
+    html = await page.evaluate(() => document.body.innerHTML);
+    await expect(html).toMatch('Digite um nome válido');
+    await expect(html).toMatch('Digite um CPF válido');
+    await expect(html).toMatch('Este e-mail não é válido');
+    await expect(html).toMatch('Digite uma senha maior que 6 dígitos');
+    browser.close();
+  }, 160000);
 });
