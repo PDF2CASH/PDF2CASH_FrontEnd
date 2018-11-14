3f3eaf2c90c28adcc61f90b4870d8772aa9c9d21
Lucas Silva <lucas.gomesgs0@gmail.com>
2018-11-14 12:03:58 -0200

    Save packages to test and test list invoice
    refs: fga-eps-mds/2018.2-PDF2CASH#141
    
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

---
+++ pdf2cash_front/src/tests/invoice.test.js
@@ -0,0 +1,27 @@
+import puppeteer from "puppeteer";
+
+describe('invoices pages tests', () => {
+    test('test invoices list', async () => {
+        let browser = await puppeteer.launch({
+            args: ['--no-sandbox']
+        });
+        let page = await browser.newPage();
+
+        page.emulate({
+            viewport: {
+                width: 500,
+                height: 2400,
+            },
+            userAgent: ''
+        });
+
+        await page.goto(APP);
+        let html = await page.evaluate(() => document.body.innerHTML);
+        expect(html).toMatch('Listar Notas Fiscais');
+        expect(html).toMatch('00000000000000000000000000000000000000000000');
+        browser.close();
+    }, 160000);
+
+    
+
+});
