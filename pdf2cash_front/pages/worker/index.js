6a7da1099b5c9adfee21ae79c0e7c0d2d1ce6e2f
Wictor Bastos Girardi <wictor.girardi@gmail.com>
2018-11-19 22:08:14 -0200

    bug correction and fetch update on worker index
                refs: fga-eps-mds/2018.2-PDF2CASH#152
                Co-authored-by: Bruno Matias <brunomatias0422@gmail.com>

--- pdf2cash_front/pages/worker/index.js
+++ pdf2cash_front/pages/worker/index.js
@@ -1,28 +1,26 @@
+
 import React, { Component } from 'react';
-import {
-  TableRow,
-  TableCell,
-  Button,
-  Grid,
-  Modal,
-  Typography,
-} from '@material-ui/core';
+import CustomatizedTable from '../../comps/tableWorker';
+import { TableRow, TableCell, Button, Grid } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import CreateIcon from '@material-ui/icons/Create'
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Link from 'next/link'
-import CustomatizedTable from '../../comps/tableWorker';
+import Modal from '@material-ui/core/Modal';
+import Authenticate  from '../auth';
+
 
 const styles = theme => ({
   cell: {
-    textAlign: 'center',
+    textAlign: 'center'
   },
   paper: {
     position: 'absolute',
     width: theme.spacing.unit * 50,
     backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[ 5 ],
+    boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
     textAlign: 'center',
   },
@@ -40,13 +38,14 @@ function getModalStyle() {
   const left = 50;
 
   return {
-    top: `${ top }%`,
-    left: `${ left }%`,
-    transform: `translate(-${ top }%, -${ left }%)`,
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
   };
 }
 
-class WorkerIndex extends Component {
+class WorkerIndex extends Component{
+
   constructor(props) {
     super(props);
     this.state = {
@@ -61,127 +60,135 @@ class WorkerIndex extends Component {
   }
 
   async componentDidMount() {
-    const url = 'http://localhost:8008/api/worker/worker/';
-    const res = await fetch(url);
-    const dataWorkers = await res.json();
+    const url = 'http://localhost:8000/api/worker/worker/';
+    const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+         'Content-Type': 'application/json',
+         'Authorization': 'JWT ' + Authenticate.getToken()
+    },
+    credentials: 'omit',
+  });
+    const data_workers = await res.json();
     this.setState({
-      workers: dataWorkers,
+      workers: data_workers,
       open: false,
     });
   }
 
   async getWorkers() {
-    const url = 'http://localhost:8008/api/worker/worker/';
-    const res = await fetch(url);
-    const dataWorkers = await res.json();
-    this.setState({ workers: dataWorkers });
+    const url = 'http://localhost:8000/api/worker/worker/';
+    const res = await fetch(url)
+    const data_workers = await res.json();
+    this.setState({ workers: data_workers });
   }
 
-  async delete() {
-    const { id } = await this.state;
-    const url = `http://localhost:8008/api/worker/worker/${ id }/`;
-    await fetch(url, { method: 'DELETE' });
+  async delete(){
+    const id = await this.state.id;
+    const url = 'http://localhost:8000/api/worker/worker/'+ id + '/';
+    const res = await fetch(url, { method:'DELETE' });
     this.closeModal();
     this.getWorkers();
   }
 
-  openModal(id) {
-    this.setState({
+  openModal(id){
+    this.setState ({
       open: true,
-      id,
+      id: id,
     });
   }
 
-  closeModal() {
+  closeModal(){
     this.setState({
-      open: false,
+      open: false
     });
   }
 
   render() {
     const { classes } = this.props;
-    const { open, id, workers } = this.state;
     return (
-        <Grid>
-            <Typography variant="display2">
+      <Grid>
+        <Typography variant="display2">
           Listar Funcionarios
-            </Typography>
-            <Modal
-              aria-labelledby="simple-modal-title"
-              aria-describedby="simple-modal-description"
-              open={ open }
-              onClose={ this.closeModal }
-            >
-                <Grid style={ getModalStyle() } className={ classes.paper }>
-                    <Typography variant="h6" className={ classes.cell }>
+        </Typography>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={this.closeModal}
+        >
+          <Grid style={getModalStyle()} className={classes.paper}>
+            <Typography variant='h6' className={classes.cell} >
               Deseja realmete deletar esse funcionário ?
-                    </Typography>
-                    <Button
-                      variant="contained"
-                      className={ classes.buttom }
-                      color="primary"
-                      onClick={ () => this.delete({ id }) }
-                    >
+            </Typography>
+            <Button
+              id = 'SIM'
+              className={classes.buttom}
+              color='primary'
+              onClick={() => this.delete(this.state.id)}
+            >
               SIM
-                    </Button>
-                    <Button
-                      variant="contained"
-                      className={ classes.buttom }
-                      color="secondary"
-                      onClick={ this.closeModal }
-                    >
+            </Button>
+            <Button
+              id = 'NAO'
+              className={classes.buttom}
+              color='secondary'
+              onClick={this.closeModal}
+            >
               NÃO
+            </Button>
+          </Grid>
+        </Modal>
+        {
+          this.state.workers.length ? (
+        <CustomatizedTable>
+          {
+            this.state.workers.map(worker => (
+              <TableRow key={worker.id}>
+                <TableCell className={classes.cell}>
+                  <Typography>
+                    {worker.username}
+                  </Typography>
+                </TableCell>
+                <TableCell className={classes.cell}>
+                  <Typography>
+                    {worker.cpf}
+                  </Typography>
+                </TableCell>
+                <TableCell className={classes.cell}>
+                  <Link href={{ pathname: '/worker/show', query: { id: worker.id } }}>
+                    <Button>
+                      <VisibilityIcon />
                     </Button>
-                </Grid>
-            </Modal>
-            {
-          workers.length ? (
-              <CustomatizedTable>
-                  {
-            workers.map(worker => (
-                <TableRow key={ worker.id }>
-                    <TableCell className={ classes.cell }>
-                        <Typography>
-                            {worker.name}
-                        </Typography>
-                    </TableCell>
-                    <TableCell className={ classes.cell }>
-                        <Typography>
-                            {worker.cpf}
-                        </Typography>
-                    </TableCell>
-                    <TableCell className={ classes.cell }>
-                        <Link href={ { pathname: '/worker/show', query: { id: worker.id } } }>
-                            <Button>
-                                <VisibilityIcon />
-                            </Button>
-                        </Link>
-                    </TableCell>
-                    <TableCell className={ classes.cell }>
-                        <Link href={ { pathname: '/worker/edit', query: { id: worker.id } } }>
-                            <Button>
-                                <CreateIcon />
-                            </Button>
-                        </Link>
-                    </TableCell>
-                    <TableCell className={ classes.cell }>
-                        <Button onClick={ () => this.openModal(worker.id) }>
-                            <DeleteIcon />
-                        </Button>
-                    </TableCell>
-                </TableRow>
+                  </Link>
+                </TableCell>
+                <TableCell className={classes.cell}>
+                  <Link href={{ pathname: '/worker/edit', query: { id: worker.id } }}>
+                    <Button
+                    id = 'EDIT'
+                    >
+                      <CreateIcon />
+                    </Button>
+                  </Link>
+                </TableCell>
+                <TableCell className={classes.cell}>
+                  <Button onClick={() => this.openModal(worker.id)}>
+                    <DeleteIcon />
+                  </Button>
+                </TableCell>
+              </TableRow>
             ))
           }
-              </CustomatizedTable>
-          ) : (
-              <Grid className={ classes.warning }>
-                  <Typography variant="display1">
+        </CustomatizedTable>
+      ) : (
+      <Grid className = {classes.warning}>
+        <Typography variant="display1">
           Não há funcionários cadastrados!
-                  </Typography>
-              </Grid>
-          )
+        </Typography>
+      </Grid>
+      )
     }
-        </Grid>
+      </Grid>
     )
   }
 }
