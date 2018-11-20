41eea6d87d6c4a9b872c1c7a3cf0850c5f2d3f64
Daniel Marques <danielmarques7@hotmail.com>
2018-11-20 15:39:44 +0000

    Add fetch of bar chart
    refs fga-eps-mds/2018.2-PDF2CASH#163
    
    Co-authored-by: jclitwin <j.litwin@live.com>
    Co-authored-by: Wictorgirardi <wictor.girardi@gmail.com>

--- pdf2cash_front/pages/index.js
+++ pdf2cash_front/pages/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Authenticate  from './auth';
 import { Grid } from '@material-ui/core';
 import { Bar } from 'react-chartjs-2';
 
-const styles = theme => ({
-
+const styles = ({
+    chart: {
+        width: '100%',
+        maxWidth: '100%',
+    },
+    grid: {
+        overflowX: 'auto',
+        overflowY: 'auto'
+    },
 });
 
 const data = {
@@ -18,34 +26,59 @@ const data = {
         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
         hoverBorderColor: 'rgba(255,99,132,1)',
         data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: 'teste',
+        backgroundColor: 'rgba(255,15,50,200)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [25, 19, 40, 31, 16, 25, 20]
       }
     ]
   };
 
-class Index extends React.Component {
+class Index extends Component {
 
     constructor(props) {
         super(props);
-        this.state= [];
+        this.state = {
+            invoices: []
+        };
+    }
+
+    async componentDidMount2() {
+        const url = 'http://localhost:8000/invoice/invoice/'
+        const head = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'JWT ' + Authenticate.getToken()
+            },
+            credentials: 'omit',
+        }
+        const response = await fetch(url, head);
+        const data = await response.json();
+        this.setState({ invoices: data });
     }
 
     render() {
+        const classes = this.props;
 
         return (
-            <Grid>
+            <Grid className={classes.grid}>
                 <Bar
                     data={data}
-                    width={100}
-                    height={400}
+                    height={'250px'}
+                    classesName={classes.chart}
                     options={{
                         maintainAspectRatio: false
                     }}
                 />
             </Grid>
         );
-
     }
-
 }
 
 export default withStyles(styles)(Index);
