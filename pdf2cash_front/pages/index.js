5881b04151d5f5b90851251b48894e47d6f07c95
Daniel Marques <danielmarques7@hotmail.com>
2018-11-21 03:09:08 +0000

    Add fetch of endpoint chart
    refs fga-eps-mds/2018.2-PDF2CASH#163
    
    Co-authored-by: jclitwin <j.litwin@live.com>
    Co-authored-by: Wictorgirardi <wictor.girardi@gmail.com>

--- pdf2cash_front/pages/index.js
+++ pdf2cash_front/pages/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Authenticate  from './auth';
 import { Grid } from '@material-ui/core';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 const styles = ({
     chart: {
@@ -15,41 +15,17 @@ const styles = ({
     },
 });
 
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-        label: 'teste',
-        backgroundColor: 'rgba(255,15,50,200)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1,
-        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-        hoverBorderColor: 'rgba(255,99,132,1)',
-        data: [25, 19, 40, 31, 16, 25, 20]
-      }
-    ]
-  };
-
 class Index extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            invoices: []
+            chart_data: {}
         };
     }
 
-    async componentDidMount2() {
-        const url = 'http://localhost:8000/invoice/invoice/'
+    async componentDidMount() {
+        const url = 'http://localhost:8008/api/invoice/char_total_value_per_time/';
         const head = {
             method: 'GET',
             headers: {
@@ -58,18 +34,33 @@ class Index extends Component {
             },
             credentials: 'omit',
         }
-        const response = await fetch(url, head);
-        const data = await response.json();
-        this.setState({ invoices: data });
+        const res = await fetch(url, head);
+        const data = await res.json();
+        const chart_data = await {
+            labels: data.date,
+            datasets: [
+                {
+                    label: 'Valor total/Tempo',
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: data.total
+                },
+            ]
+        }
+        await this.setState({ chart_data });
     }
 
     render() {
         const classes = this.props;
+        const { chart_data } = this.state;
 
         return (
             <Grid className={classes.grid}>
-                <Bar
-                    data={data}
+                <Line
+                    data={chart_data}
                     height={'250px'}
                     classesName={classes.chart}
                     options={{
